3b0d6425100ec65c1eb00f23e6e11d3b6e94f762
qidianwei <vin_7@qq.com>
2025-03-17 09:56:10 +0800

    feat: 修改路由跳转

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -7,9 +8,21 @@ import ProjectDetail from './components/ProjectDetail'
 import Contact from './components/Contact'
 import './App.css'
 
+// 滚动重置组件
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={
